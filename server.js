c1433eb5327099e41f3d2faa0aa698880239bd57
Frank Zhang <frankbusinessmail@gmail.com>
2019-05-29 07:58:53 -0400

    playing with feedly nodejs library

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const authRoutes = require('./routes/authN');
+const Feedly = require('feedly');
+
 
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(express.json());
 
+
+const f = new Feedly({
+    client_id: 'sandbox',
+    client_secret: 'YamL84gadHERJNcQ',
+    base: 'http://sandbox7.feedly.com',
+    port: 8080
+})
+
+f.categories((error, category) => {
+    console.log(category);
+    
+})
+
+f.logout();
+
 app.use('/', authRoutes)
-app.listen(8080, console.log(`server successfully set up.`));
+app.listen(8888, console.log(`server successfully set up.`));
 
